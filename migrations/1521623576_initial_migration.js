eea8a7f63f3229a02febbfca4b98a08d19a0aec1
Michał Dulat <michal@dulat.pl>
2018-03-21 11:10:46 +0100

    Skip migrations in tests

--- migrations/1521623576_initial_migration.js
+++ migrations/1521623576_initial_migration.js
@@ -1,5 +1,6 @@
 const Migrations = artifacts.require('./Migrations.sol')
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
+  if (network === 'testrpc') return
   deployer.deploy(Migrations)
 }
--- migrations/1521623592_deploy_token_contract.js
+++ migrations/1521623592_deploy_token_contract.js
@@ -1,5 +1,6 @@
 const CharityToken = artifacts.require('CharityToken')
 
 module.exports = function(deployer, network) {
+  if (network === 'testrpc') return
   deployer.deploy(CharityToken)
 }
